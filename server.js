81674a5b3f434bd9e53c881a7ab2087988013760
athalafk <athalafarastya@gmail.com>
2025-03-30 21:47:27 +0700

    add root route

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const authRoutes = require('./routes/authRoutes');
 const barangRoutes = require('./routes/barangRoutes');
 const riwayatRoutes = require('./routes/riwayatRoutes');
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Selamat datang di aplikasi kami!');
+  });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/barang', barangRoutes);
 app.use('/api/riwayat', riwayatRoutes);
 
 const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0'; // Listen di semua alamat
+const HOST = '0.0.0.0'; 
 
 app.listen(PORT, HOST, () => console.log(`Server berjalan di http://${HOST}:${PORT}`));
 
