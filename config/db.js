7b6d1c6034ed6bd9568f9cd9c1c53718e65ab4ae
athalafk <athalafarastya@gmail.com>
2025-03-30 16:51:31 +0700

    Update connection

--- config/db.js
+++ config/db.js
@@ -1,20 +1,25 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST, 
-  port: process.env.MYSQLPORT,  
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+console.log("ENV CHECK:", process.env.MYSQL_HOST, process.env.MYSQL_PORT, process.env.MYSQL_USER);
+
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error('Database connection error:', err);
     return;
   }
   console.log('Database connected');
+  connection.release();
 });
 
 module.exports = db;
